d3cb3280b7f564d9b276191841bf995a28b5300d
yonmoyonmo <yonmosound@gmail.com>
2022-10-03 18:37:56 +0900

    test : env file, mysql drivers, third party dependencies

--- cmd/simpleGoHttpPractice/main.go
+++ cmd/simpleGoHttpPractice/main.go
@@ -4,9 +4,57 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
+	"time"
+
+	"database/sql"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
+func GetConnector() *sql.DB {
+	cfg := mysql.Config{
+		User:                 os.Getenv("USER_NAME"),
+		Passwd:               os.Getenv("PASSWORD"),
+		Net:                  "tcp",
+		Addr:                 os.Getenv("HOST"),
+		Collation:            "utf8mb4_general_ci",
+		Loc:                  time.UTC,
+		MaxAllowedPacket:     4 << 20.,
+		AllowNativePasswords: true,
+		CheckConnLiveness:    true,
+		DBName:               os.Getenv("DB_NAME"),
+	}
+	connector, err := mysql.NewConnector(&cfg)
+	if err != nil {
+		panic(err)
+	}
+	db := sql.OpenDB(connector)
+	return db
+}
+
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	log.Println(os.Getenv("TEST"))
+	db := GetConnector()
+
+	err2 := db.Ping()
+	if err != nil {
+		log.Println(err2)
+	}
+
+	var connectionTest string
+	err = db.QueryRow("SELECT sugguestion_text FROM sugguestion WHERE id =12").Scan(&connectionTest)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(connectionTest)
+	print("success")
+
 	helloHandler := func(w http.ResponseWriter, req *http.Request) {
 		io.WriteString(w, "Hello, world!\n")
 	}
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/yonmoyonmo/simpleGoHttpPractice
 
 go 1.19
+
+require github.com/joho/godotenv v1.4.0
+
+require github.com/go-sql-driver/mysql v1.6.0 // indirect
